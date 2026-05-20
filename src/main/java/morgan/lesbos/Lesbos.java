192d22c89d08c0280c51d751dee225c9e971cccb
Morganpitta <morganpittaway24@outlook.com>
2026-05-20 22:39:37 +0100

    Removed inventory stuff and added origins as dependency

--- src/main/java/morgan/lesbos/Lesbos.java
+++ src/main/java/morgan/lesbos/Lesbos.java
@@ -1,23 +1,12 @@
 package morgan.lesbos;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.EntityEquipment;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.collection.DefaultedList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.EnumMap;
-
 public class Lesbos implements ModInitializer {
-    // This logger is used to write text to the console and the log file.
-    // It is considered best practice to use your mod id as the logger's name.
-    // That way, it's clear which mod wrote info, warnings, and errors.
     public static final String MOD_ID = "Lesbos";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static DefaultedList<ItemStack> globalInventoryMain;
-    public static EnumMap<EquipmentSlot, ItemStack> globalInventoryMap;
 
     @Override
     public void onInitialize() {
