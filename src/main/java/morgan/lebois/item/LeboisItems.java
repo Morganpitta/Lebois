c9f50426981d712de0db535b56b5ae356953a40d
Morganpitta <morganpittaway24@outlook.com>
2026-07-03 00:12:58 +0100

    A bit of funnies

--- src/main/java/morgan/lebois/item/LeboisItems.java
+++ src/main/java/morgan/lebois/item/LeboisItems.java
@@ -1,13 +1,21 @@
 package morgan.lebois.item;
 
 import morgan.lebois.Lebois;
+import net.minecraft.block.jukebox.JukeboxSongs;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.component.type.FoodComponents;
+import net.minecraft.component.type.JukeboxPlayableComponent;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Rarity;
 
 public class LeboisItems {
-    public static final Item COIN = register("coin", new Item(new Item.Settings()));
+    public static final Item PATRICK = register("patrick", new Item(
+        new Item.Settings().rarity(Rarity.EPIC).food(FoodComponents.ENCHANTED_GOLDEN_APPLE).component(DataComponentTypes.ENCHANTMENT_GLINT_OVERRIDE, true).fireproof().jukeboxPlayable(JukeboxSongs.PIGSTEP)
+    ));
 
     public static void register() {
     }
