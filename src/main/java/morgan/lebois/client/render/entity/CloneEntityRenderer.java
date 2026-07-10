5aa7cea773a913d7ef9ae317976c13765816e88c
Morganpitta <morganpittaway24@outlook.com>
2026-07-11 00:37:12 +0100

    Clone entity tweaks

--- src/main/java/morgan/lebois/client/render/entity/CloneEntityRenderer.java
+++ src/main/java/morgan/lebois/client/render/entity/CloneEntityRenderer.java
@@ -1,8 +1,10 @@
 package morgan.lebois.client.render.entity;
 
+import morgan.lebois.Lebois;
 import morgan.lebois.entity.CloneEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.BipedEntityRenderer;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.feature.*;
@@ -10,7 +12,9 @@ import net.minecraft.client.render.entity.model.ArmorEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelLayers;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.client.util.DefaultSkinHelper;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
 
 @Environment(EnvType.CLIENT)
 
@@ -29,7 +33,24 @@ public class CloneEntityRenderer extends BipedEntityRenderer<CloneEntity, Player
     }
 
     @Override
+    public void render(CloneEntity clone, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+        this.setModelPose(clone);
+        super.render(clone, f, g, matrixStack, vertexConsumerProvider, i);
+    }
+
+    @Override
+    public Vec3d getPositionOffset(CloneEntity clone, float f) {
+        return clone.isSitting()
+                ? new Vec3d(0.0, clone.getScale() * -2.0F / 16.0, 0.0)
+                : super.getPositionOffset(clone, f);
+    }
+
+    private void setModelPose(CloneEntity clone) {
+        this.model.sneaking = clone.isSitting();
+    }
+
+        @Override
     public Identifier getTexture(CloneEntity entity) {
-        return DefaultSkinHelper.getTexture();
+        return Lebois.id("textures/entity/skin.png");
     }
 }
